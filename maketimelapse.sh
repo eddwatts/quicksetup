31b3f8ddff93e0925382f986eb25ab544b05ad28
eddwatts <edd@eddtech.co.uk>
2022-06-15 09:41:15 +0100

    Create maketimelapse.sh

---
+++ maketimelapse.sh
@@ -0,0 +1,11 @@
+echo '[Unit]' | sudo tee --append /etc/systemd/system/timelapse.service
+echo 'Description=My Script' | sudo tee --append /etc/systemd/system/timelapse.service
+echo '' | sudo tee --append /etc/systemd/system/timelapse.service
+echo '[Service]' | sudo tee --append /etc/systemd/system/timelapse.service
+echo 'ExecStart=/usr/bin/python3 /home/timelapse.py' | sudo tee --append /etc/systemd/system/timelapse.service
+echo '' | sudo tee --append /etc/systemd/system/timelapse.service
+echo '[Install]' | sudo tee --append /etc/systemd/system/timelapse.service
+echo 'WantedBy=multi-user.target' | sudo tee --append /etc/systemd/system/stimelapse.service
+sudo curl -o "/home/timelapse.py" "https://raw.githubusercontent.com/eddwatts/quicksetup/master/timelapse.py?id=$RANDOM" -L
+sudo systemctl start timelapse    # Runs the script now
+sudo systemctl enable timelapse   # Sets the script to run every boot

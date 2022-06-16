d6f011e595b5257b4bcb82da5722608d2e9f77c8
eddwatts <edd@eddtech.co.uk>
2022-06-16 16:11:02 +0100

    Update safesuntdown.py

--- safesuntdown.py
+++ safesuntdown.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 import time
 from datetime import datetime
 import os
-
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)

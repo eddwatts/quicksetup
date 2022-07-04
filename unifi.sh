7c52548a634f0ea869d23cdd0354cf7d08eef5c3
eddwatts <edd@eddtech.co.uk>
2022-07-04 10:03:41 +0100

    Update unifi.sh

--- unifi.sh
+++ unifi.sh
@@ -1,4 +1,28 @@
-raspi-config nonint do_boot_wait 0
+echo 'dtparam=watchdog=on' | sudo tee --append /boot/config.txt
+echo 'dtoverlay=vc4-kms-v3d,cma-128' | sudo tee --append /boot/config.txt
+echo 'disable_camera_led=1' | sudo tee --append /boot/config.txt
+sudo mkdir /mnt/ramdisk
+echo 'tmpfs /mnt/ramdisk tmpfs nodev,nosuid,size=20M 0 0' | sudo tee --append /etc/fstab
+sudo mount -a
+sudo raspi-config nonint do_boot_wait 0
+sudo raspi-config nonint do_memory_split 128
+sudo raspi-config nonint do_camera 0
+sudo raspi-config nonint do_ssh 0
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_boot_wait 0
+sudo raspi-config nonint do_configure_keyboard gb
+sudo raspi-config nonint do_wifi_country GB
+sudo raspi-config nonint do_change_timezone Eurpoe/London
+sudo raspi-config nonint do_change_locale en_GB.UTF-8
+echo '[Unit]' | sudo tee --append /etc/systemd/system/safeshutdown.service
+echo 'Description=Shutdown service' | sudo tee --append /etc/systemd/system/safeshutdown.service
+echo '' | sudo tee --append /etc/systemd/system/safeshutdown.service
+echo '[Service]' | sudo tee --append /etc/systemd/system/safeshutdown.service
+echo 'ExecStart=/usr/bin/python3 /home/safeshutdown.py' | sudo tee --append /etc/systemd/system/safeshutdown.service
+echo '' | sudo tee --append /etc/systemd/system/safeshutdown.service
+echo '[Install]' | sudo tee --append /etc/systemd/system/safeshutdown.service
+echo 'WantedBy=multi-user.target' | sudo tee --append /etc/systemd/system/safeshutdown.service
+sudo curl -o "/home/safeshutdown.py" "https://raw.githubusercontent.com/eddwatts/quicksetup/main/safesuntdown.py?id=$RANDOM" -L
 echo '#Never Prefer packages from Stretch' | sudo tee --append /etc/apt/preferences.d/99stretch-mongodb.pref
 echo 'Package: *' | sudo tee --append /etc/apt/preferences.d/99stretch-mongodb.pref
 echo 'Pin: release n=stretch' | sudo tee --append /etc/apt/preferences.d/99stretch-mongodb.pref
@@ -26,7 +50,18 @@ echo '-A FORWARD -j DROP' | sudo tee --append /etc/iptables.firewall.rules
 sudo iptables-restore < /etc/iptables.firewall.rules
 sudo apt update
 sudo apt upgrade
-sudo apt install -y openjdk-8-jre-headless rng-tools
+sudo apt install -y openjdk-8-jre-headless rng-tools python3-pip watchdog
 sed -i 's/#HRNGDEVICE=/HRNGDEVICE=/' /etc/default/rng-tools-debian
 sudo systemctl restart rng-tools
 sudo apt install unifi
+
+echo 'watchdog-device = /dev/watchdog' | sudo tee --append /etc/watchdog.conf
+echo 'watchdog-timeout = 15' | sudo tee --append /etc/watchdog.conf
+echo 'max-load-1 = 24' | sudo tee --append /etc/watchdog.conf
+echo 'interface = eth0' | sudo tee --append /etc/watchdog.conf
+echo 'ping = 1.1.1.1' | sudo tee --append /etc/watchdog.conf
+echo 'realtime = yes' | sudo tee --append /etc/watchdog.conf
+sudo systemctl start safeshutdown    # Runs the script now
+sudo systemctl enable safeshutdown   # Sets the script to run every boot
+sudo systemctl enable watchdog
+sudo systemctl start watchdog
